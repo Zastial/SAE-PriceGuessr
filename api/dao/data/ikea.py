56c99d31a6d8e087f63a6b2b4ed9973ae3f1d981
Louis <louis.painter@etu.univ-nantes.fr>
2023-03-24 16:05:05 +0100

    ajout startPopulate

--- api/dao/data/ikea.py
+++ api/dao/data/ikea.py
@@ -5,7 +5,7 @@ import random
 import time
 
 constants = ikea_api.Constants(country="fr", language="fr")
-ikeaProducts = ikea_api.run(ikea_api.Search(constants).search("",limit=100))['searchResultPage']['products']['main']['items']
+ikeaProducts = ikea_api.run(ikea_api.Search(constants).search("",limit=50_000))['searchResultPage']['products']['main']['items']
 random.seed()
 ret = []
 
