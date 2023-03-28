4fb83cb32eeb13dac3970b08023762a74a157e6c
E21B036Z <E21B036Z@univ-nantes.fr>
2023-03-28 16:57:25 +0200

    game init

--- guessr_web/test-app/src/components/page/GameInterface.mjs
+++ guessr_web/test-app/src/components/page/GameInterface.mjs
@@ -1,32 +1,60 @@
 import '../style/game.css'
-import React from "react";
+import React, {useState} from "react";
 import Button from '../Button.mjs'
 import {getProducts, getProductById, getDailyProducts, guessThePrice, getProductsByDate, register, login, deleteUser, modifyUser} from '../API.mjs';
-  
+
 class GameInterface extends React.Component {
 
     constructor(props) {
         super(props)
-        this.doUpdate = this.doUpdate.bind(this)
         this.state = {
-            products: []
+            indexProduit:0,
+            count:0,
+            produits:[],
+            produitCourant:[]
         }
+        this.produits = []
+        this.doUpdate = this.doUpdate.bind(this)
+        this.before = this.before.bind(this)
+        this.after = this.after.bind(this)
     }
 
     async componentDidMount() {
-        const arr = await getProducts(sessionStorage.getItem("jwt"))
-
-        for (let val in arr[0]) {
-            console.log(val)
-        }
-
-        console.log(`array : ${arr[0]['title']}`)
-        // this.setState({products : arr})
-        // console.log(this.state.products)
+        this.produits = await getProducts(sessionStorage.getItem("jwt"))
+        
+        this.setState({
+            produits:[this.produits],
+            produitCourant:this.produits[this.state.indexProduit]
+        })
     }
 
+    before() {
+        console.log(this.state.indexProduit)
+        if(this.state.indexProduit === 0) {
+            return
+        }
+        this.setState({
+            indexProduit : this.state.indexProduit - 1,
+            produitCourant:this.produits[this.state.indexProduit-1]
+        })
+        console.log(this.state.indexProduit)
+    }
+
+    after() {
+        console.log(this.state.indexProduit)
+        if(this.state.indexProduit === 9) {
+            return
+        }
+        this.setState({
+            indexProduit: this.state.indexProduit + 1,
+            produitCourant: this.produits[this.state.indexProduit+1]
+        })
+        console.log(this.state.indexProduit)
+    }
+
+
     doUpdate() {
-        return 
+        this.produits = guessThePrice(sessionStorage.getItem("jwt")) 
     }
 
     render() {
@@ -34,13 +62,17 @@ class GameInterface extends React.Component {
             <div className="gameInterface">
                 <div className="game-info">
                     <p> Quel est le prix de cet objet ? </p>
-                    <p> {this.state.products} </p>
+                    <p> {this.state.produitCourant.title} </p>
                 </div>
                 <div className="game-guess">
-                    <img className="product-picture" src="./ikea.png" alt="description produit ikea"/>
+                    <div className="guess-info">
+                        <Button name="<" doUpdate={this.before} />
+                        <img className="product-picture" src={this.state.produitCourant.imgSrc}alt="description produit ikea"/>
+                        <Button name=">" doUpdate={this.after} /> 
+                    </div>
                     <p> 1 chance sur 5 </p>
                     <div className="the-guess">
-                        <input/>
+                        <input type="number" value={this.state.count} onChange={e => {this.setState({count : "" + Number(e.target.value)});}}/>
                         <p>€</p>
                     </div>
                 </div>
